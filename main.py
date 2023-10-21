72b0deabefdabe0327b5d7410ba4bec98c9c0f55
classmain <classmain@qq.com>
2023-10-21 11:29:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -167,13 +167,12 @@ def login(user, password):
         }
     r2 = requests.post(url2, data=data2, headers=headers).json()
     login_token = r2["token_info"]["login_token"]
-     print("login_token获取成功！")
-     print(login_token)
+    print("login_token获取成功！")
+    print(login_token)
     userid = r2["token_info"]["user_id"]
     print("userid获取成功！")
     print(userid)
-
- print("用户名："+user+",密码："+password)
+    print("用户名："+user+",密码："+password)
     return login_token, userid
 
 
